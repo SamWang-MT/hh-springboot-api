b63f63ccd332633fcdb9000c7f8c80f60374d95e
SamWang-MT <samwang.mt@gmail.com>
2021-01-12 16:23:17 -0500

    add timer TimeTasks.java

--- src/main/java/com/halcyon/file_manage/task/TimedTasks.java
+++ src/main/java/com/halcyon/file_manage/task/TimedTasks.java
@@ -9,6 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling //开始执行定时任务，
 	   public class TimedTasks {
+	/**
+		（4）0 0 1,12,18 * * ? 每天上午1点，12点，18点
+ 	
+ 		（6）0 0 12 ? * WED 表示每个星期三中午12点
+ 		（7）0 0 12 * * ? 每天中午12点触发
+	 */
+	
 	  	 	//添加定时任务
 	  	 	//每秒递增运行    0,5,10,15
 	  	 	@Scheduled(cron = "0/5 * * * * ?") //@Scheduled注解 表示此方法是一个定时任务方法
